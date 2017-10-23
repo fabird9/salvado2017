b0b9587a7ff7327f465c974e0080c94ef5a6e70a
pabloscooby <pabloscooby@gmail.com>
2017-10-23 19:56:12 +0200

    Añadida mayor conversación
    Para la motivación personal

--- saluda.java
+++ saluda.java
@@ -1,6 +1,15 @@
 Class saluda{
 public static void main(String[]args){
-     System.out.println("Hola que tal estas?");
+     System.out.println("-Hola que tal estas?");
+     System.out.println("-Mal :(");
+          for(int i = 0; i < 10 ; i++){
+               if(i = 0){
+                    System.out.println("-Ánimo tú puedes!");
+               }
+               else{
+                    System.out.println("Ánimo tú puedes!");
+               }
+          }
 
 
 }
